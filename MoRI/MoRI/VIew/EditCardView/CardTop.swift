b420dae030b387911e19dc5f39d1d392c7458ec4
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-19 13:56:15 +0900

    ✨ FEAT: EditCard View
    카드를 그려냄.
    색상 Asset에 추가 작업 필요
    폰트 적용 필요
    Asset에 이미지 추가 필요

--- MoRI/MoRI/VIew/EditCardView/CardTop.swift
+++ MoRI/MoRI/VIew/EditCardView/CardTop.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct CardTop: View {
+    @StateObject var viewModel: EditCardViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0){
+            ZStack{
+                Image(uiImage: viewModel.card.albumArtUIImage)
+                    .resizable()
+                    .frame(width: 350, height: 350)
+                    .cornerRadius(300, corners: .topRight)
+                    .cornerRadius(30, corners: .topLeft)
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .padding(.top, -160)
+                    .padding(.leading, -160)
+                    .blendMode(.destinationOut)
+                Button(action: {print("pick color")}){
+                    ZStack{
+                        Circle()
+                        .frame(width: 28, height: 28)
+                        .foregroundColor(.white)
+                        Image("dropper")
+                    }
+                }
+                .padding(.top, 280)
+                .padding(.leading, 300)
+
+            }
+            .compositingGroup()
+            ZStack(alignment: .top){
+                Rectangle()
+                    .frame(width: 350, height: 74)
+                    .cornerRadius(20, corners: [.bottomLeft, .bottomRight])
+                    .foregroundColor(viewModel.card.cardColor)
+                VStack(spacing: 0){
+                    Text(viewModel.card.title)
+                        .font(.system(size: 34, weight: .medium))
+                        .foregroundColor(viewModel.lyricsColor)
+                        .frame(height: 34)
+                    Text(viewModel.card.singer + "-" + viewModel.card.date)
+                        .frame(width: 314, height: 22, alignment: .leading)
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundColor(viewModel.lyricsColor)
+                        .blendMode(.softLight)
+                }
+                .padding(.top, 17)
+            }.compositingGroup()
+        }
+        .onAppear{
+            viewModel.getColorFromImagePixel()
+        }
     }
 }
 
 struct CardTop_Previews: PreviewProvider {
     static var previews: some View {
-        CardTop()
+        CardTop(viewModel: EditCardViewModel(card: Card(albumArtUIImage: UIImage(named: "test") ?? UIImage(), title: "커다란", singer: "민수", lyrics: "사랑은 보이지 않는 곳에 흔적을 남기지\n사람은 고스란히 느낄 수가 있지\n서로를 향하는 마음이 진심인지\n참 신기하게도 알 수가 있어", cardColor: .clear)))
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
     }
 }
