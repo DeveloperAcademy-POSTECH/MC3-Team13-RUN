969135a6d76516cc1621e434e25959fa053fb120
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-07-19 11:11:47 +0900

    ✨FEAT: #5 url에서 HTML 가사부분 가져오는 함수 구현

---
+++ MoRI/MoRI/VIewModel/SelectLyricsViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  SelectLyricsViewModel.swift
+//  MoRI
+//
+//  Created by GYURI PARK on 2023/07/19.
+//
+
+import Foundation
+import SwiftSoup
+
+class SearchLyricsViewModel: ObservableObject {
+    @Published var lyrics: [[String]] = []
+    
+    init() {
+        fetchHTMLParsingResult(artist: "", title: "")
+    }
+    
+    // MARK: 해당 url의 HTML 태그를 가져옵니다.
+    func fetchHTMLParsingResult(artist: String, title: String) {
+        let urlAddress = "https://genius.com/\(artist)-\(title)-lyrics"
+        guard let url = URL(string: urlAddress) else {
+            return
+        }
+        
+        DispatchQueue.global().async {
+            do {
+                let html = try String(contentsOf: url, encoding: .utf8)
+                let doc: Document = try SwiftSoup.parse(html)
+                
+                let container = try doc.select(".Lyrics__Container-sc-1ynbvzw-5.Dzxov")
+                var lyricsArray = [[String]]()
+                
+                for element in container.array() {
+                    let brTags = try element.select("br")
+                    if brTags.array().isEmpty {
+                        lyricsArray.append([try element.text()])
+                    } else {
+                        for brTag in brTags.array() {
+                            try brTag.after("\n")
+                        }
+                        let lines = try element.text().components(separatedBy: "\n")
+                        lyricsArray.append(lines)
+                    }
+                }
+                
+                DispatchQueue.main.async {
+                    self.lyrics = lyricsArray
+                }
+                
+            } catch {
+                print("Error parsing HTML: \(error)")
+            }
+        }
+    }
+}
