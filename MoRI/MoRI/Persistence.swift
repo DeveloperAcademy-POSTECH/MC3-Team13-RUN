9d5483e8701461acadf43107efa47e16db6ed18d
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-23 08:43:28 +0900

    ✨ FEAT: CoreData Controller
    코어데이터 저장 함수 생성
    Card 모델 있기에 CardCD로 Entities 이름 바꿈

--- MoRI/MoRI/Persistence.swift
+++ MoRI/MoRI/Persistence.swift
@@ -6,7 +6,50 @@
 //
 
 import CoreData
+import SwiftUI
 
 struct PersistenceController {
-    static let shared = PersistenceController()
+    static public let shared = PersistenceController()
+        
+    private let container: NSPersistentContainer
+    
+    init() {
+        print("container 연결")
+        container = NSPersistentContainer(name: "MoRI")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+    
+    public func addItem(_ viewContext: NSManagedObjectContext, _ albumArt: UIImage, _ title: String, _ singer: String, _ date: String, _ lyrics: String, _ cardColor: Color) {
+        print("addItem start")
+        let newItem = CardCD(context: viewContext)
+        newItem.albumArt = UiImageToBinary(albumArt)
+        newItem.title = title
+        newItem.singer = singer
+        newItem.date = date
+        newItem.lyrics = lyrics
+        newItem.cardColorR = cardColor.components.r
+        newItem.cardColorG = cardColor.components.g
+        newItem.cardColorB = cardColor.components.b
+        newItem.cardColorA = cardColor.components.a
+
+        do {
+            try viewContext.save()
+            print("save")
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+}
+
+extension PersistenceController {
+    private func UiImageToBinary(_ uiImage: UIImage)->Data{
+        guard let data = uiImage.pngData() else { fatalError("이미지 data 추출 에러") }
+        return data
+    }
 }
