d42dea5c8427b6f27a37bac4801b0082a1138947
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-22 20:19:45 +0900

    // //  CGSize.swift //  MoRI // //  Created by Ko Seokjun on 2023/07/21. //
    import Foundation
    
    extension CGSize {
      static func + (left: Self, right: Self) -> Self {
        CGSize(width: left.width + right.width, height: left.height + right.height)
      }
    }

---
+++ MoRI/MoRI/VIew/EditCard/CardTop.swift
@@ -0,0 +1,107 @@
+//
+//  CardTop.swift
+//  MoRI
+//
+//  Created by Ko Seokjun on 2023/07/19.
+//
+
+import SwiftUI
+//112 224
+struct CardTop: View {
+    @StateObject var viewModel: EditCardViewModel
+    @State private var dragPointerIsHidden = false
+    var body: some View {
+        VStack(spacing: 0){
+            ZStack{
+                Image(uiImage: viewModel.card.albumArtUIImage)
+                    .resizable()
+                    .frame(width: 350, height: 350)
+                    .cornerRadius(300, corners: .topRight)
+                    .cornerRadius(30, corners: .topLeft)
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .padding(.top, -160)
+                    .padding(.leading, -160)
+                    .blendMode(.destinationOut)
+                Button(action: { dragPointerIsHidden.toggle() }){
+                    ZStack{
+                        Circle()
+                            .frame(width: 28, height: 28)
+                            .foregroundColor(.white)
+                        Image("dropper")
+                    }
+                }
+                .padding(.top, 280)
+                .padding(.leading, 300)
+                if dragPointerIsHidden {
+                    VStack{
+                        Circle()
+                            .frame(width: 30,height: 30)
+                            .offset(viewModel.draggedOffset)
+                            .foregroundColor(viewModel.card.cardColor)
+                        Circle()
+                            .frame(width: 15, height: 15)
+                            .offset(viewModel.draggedOffset)
+                            .opacity(0.4)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged{ gesture in
+                                        viewModel.draggedOffset = viewModel.accumulatedOffset + gesture.translation
+                                        viewModel.repositionDrag()
+                                        viewModel.getColorFromImagePixel()
+                                    }
+                                    .onEnded { gesture in
+                                        viewModel.accumulatedOffset = viewModel.accumulatedOffset + gesture.translation
+                                    }
+                            )
+                    }
+                }
+            }
+            .compositingGroup()
+            ZStack(alignment: .top){
+                Rectangle()
+                    .frame(width: 350, height: 74)
+                    .cornerRadius(20, corners: [.bottomLeft, .bottomRight])
+                    .foregroundColor(viewModel.card.cardColor)
+                VStack(spacing: 0){
+                    Text(viewModel.card.title)
+                        .font(.system(size: 34, weight: .medium))
+                        .foregroundColor(viewModel.lyricsColor)
+                        .frame(height: 34)
+                    Text(viewModel.card.singer + "-" + viewModel.card.date)
+                        .frame(width: 314, height: 22, alignment: .leading)
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundColor(viewModel.lyricsColor)
+                        .blendMode(.softLight)
+                }
+                .padding(.top, 17)
+            }.compositingGroup()
+        }
+        .onAppear{
+            viewModel.getColorFromImagePixel()
+        }
+    }
+}
+
+struct CardTop_Previews: PreviewProvider {
+    static var previews: some View {
+        CardTop(viewModel: EditCardViewModel(card: Card(albumArtUIImage: UIImage(named: "test") ?? UIImage(), title: "커다란", singer: "민수", lyrics: "사랑은 보이지 않는 곳에 흔적을 남기지\n사람은 고스란히 느낄 수가 있지\n서로를 향하는 마음이 진심인지\n참 신기하게도 알 수가 있어", cardColor: .clear)))
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
---
+++ MoRI/MoRI/VIewModel/EditCardViewModel.swift
@@ -0,0 +1,112 @@
+//
+//  EditCardViewModel.swift
+//  MoRI
+//
+//  Created by Ko Seokjun on 2023/07/19.
+//
+
+import SwiftUI
+import Combine
+
+final class EditCardViewModel: ObservableObject {
+    @Published public var card:Card
+    @Published public var lyricsContainerColor: Color = .clear
+    @Published public var lyricsColor: Color = .clear
+    @Published public var draggedOffset: CGSize
+    @Published public var accumulatedOffset = CGSize.zero
+
+    
+    init(card: Card) {
+        self.card = card
+        let image = card.albumArtUIImage
+        guard let cgImage = image.cgImage else {
+            draggedOffset = CGSize(width: 0, height: 150)
+            accumulatedOffset = CGSize(width: 0, height: 150)
+            return
+        }
+        draggedOffset = CGSize(width: 0, height: 150)
+        accumulatedOffset = CGSize(width: 0, height: 150)
+    }
+    public func getColorFromImagePixel(){
+        let image = card.albumArtUIImage
+        guard let cgImage = image.cgImage else {
+            card.cardColor = .black
+            return
+        }
+        let width = cgImage.width
+        let height = cgImage.height
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bytesPerPixel = 4
+        let bytesPerRow = bytesPerPixel * width
+        let bitmapInfo = cgImage.bitmapInfo.rawValue
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {
+            card.cardColor = .black
+            return
+        }
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        context.draw(cgImage, in: rect)
+        
+        let x = Int((draggedOffset.width+175)/350*225)
+        let y = Int((draggedOffset.height+175)/350*225)
+        let pixelData = context.data?.assumingMemoryBound(to: UInt8.self)
+        let offset = bytesPerRow * y + bytesPerPixel * x
+        let r = Double((pixelData?[offset])!)/225
+        let g = Double((pixelData?[offset + 1])!)/225
+        let b = Double((pixelData?[offset + 2])!)/225
+        card.cardColor = Color(red: r, green: g, blue: b)
+        changeColorsAfterCardColorChange()
+    }
+    
+    private func changeColorsAfterCardColorChange(){
+        let cardColor = card.cardColor
+        let r = cardColor.components.r
+        let g = cardColor.components.g
+        let b = cardColor.components.b
+        let cmax = max(r, g, b)
+        let cmin = min(r, g, b)
+        let lightness = ((cmax+cmin)/2.0)*100
+        var hue: CGFloat = 0.0
+        var saturation: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        UIColor(cardColor).getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        lyricsColor = lightness >= 60 ? .black : .white
+        if( 0.0 <= brightness && brightness < 0.70){
+            lyricsContainerColor = Color(UIColor(hue: hue, saturation: saturation, brightness: brightness+0.1, alpha: 1.0))
+        }
+        else if( 0.70 <= brightness && brightness <= 0.85 ){
+            lyricsContainerColor = Color(UIColor(hue: hue, saturation: saturation, brightness: brightness+0.15, alpha: 1.0))
+        }
+        else{
+            lyricsContainerColor = Color(UIColor(hue: hue, saturation: saturation, brightness: brightness-0.15, alpha: 1.0))
+        }
+    }
+    public func repositionDrag(){
+        if(draggedOffset.width >= 170 || draggedOffset.width <= -170 || draggedOffset.height >= 170 || draggedOffset.height <= -170 ){
+            draggedOffset = .zero
+            accumulatedOffset = .zero
+        }
+    }
+}
+
+
+extension Color {
+
+    var components: (r: Double, g: Double, b: Double, a: Double) {
+        
+        #if canImport(UIKit)
+        typealias NativeColor = UIColor
+        #elseif canImport(AppKit)
+        typealias NativeColor = NSColor
+        #endif
+        
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else { return (0,0,0,0) }
+        
+        return (Double(r), Double(g), Double(b), Double(a))
+    }
+}
