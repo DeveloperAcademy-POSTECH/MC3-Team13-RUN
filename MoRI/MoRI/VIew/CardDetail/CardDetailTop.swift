89ffb841bd15262b117b7f068575d6aed6f27158
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-23 09:22:23 +0900

    🛠️ FIX: Card
    카드 색상 받아서 값 저장

---
+++ MoRI/MoRI/VIew/CardDetail/CardDetailTop.swift
@@ -0,0 +1,20 @@
+//
+//  CardDetailTop.swift
+//  MoRI
+//
+//  Created by Ko Seokjun on 2023/07/23.
+//
+
+import SwiftUI
+
+struct CardDetailTop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardDetailTop_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDetailTop()
+    }
+}
---
+++ MoRI/MoRI/VIewModel/CardDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardDetailViewModel.swift
+//  MoRI
+//
+//  Created by Ko Seokjun on 2023/07/23.
+//
+
+import Foundation
