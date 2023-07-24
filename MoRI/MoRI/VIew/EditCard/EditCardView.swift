cf191ce90dbf8c245fcc682b3aaf7f0f20b61724
Jin-s-work <9114jin@hanmail.net>
2023-07-24 18:31:57 +0900

    ✨ FEAT: CompleteCardView 그리고 EditCardView와 연결

--- MoRI/MoRI/VIew/EditCard/EditCardView.swift
+++ MoRI/MoRI/VIew/EditCard/EditCardView.swift
@@ -15,32 +15,38 @@ struct EditCardView: View {
             
             ZStack{
                 Rectangle()
-                .frame(width: 350, height: 163) .cornerRadius(20)
-                .foregroundColor(viewModel.lyricsContainerColor)
+                    .frame(width: 350, height: 163) .cornerRadius(20)
+                    .foregroundColor(viewModel.lyricsContainerColor)
                 Rectangle()
-                .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
-                .frame(width: 314, height: 0.5)
-                .foregroundColor(viewModel.lyricsContainerColor)
-                .blendMode(.screen)
-                .padding(.top, -81)
+                    .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
+                    .frame(width: 314, height: 0.5)
+                    .foregroundColor(viewModel.lyricsContainerColor)
+                    .blendMode(.screen)
+                    .padding(.top, -81)
                 Text(viewModel.card.lyrics)
-                .frame(width: 314, alignment: .leading)
-                .foregroundColor(viewModel.lyricsColor)
-                .font(.system(size: 17, weight: .medium))
-                .lineSpacing(15)
+                    .frame(width: 314, alignment: .leading)
+                    .foregroundColor(viewModel.lyricsColor)
+                    .font(.system(size: 17, weight: .medium))
+                    .lineSpacing(15)
             }
             .compositingGroup()
-            Button(action: { print("done")} ){
-                ZStack{ Rectangle()
-                    .frame(width: 350, height: 60)
-                    .cornerRadius(30)
-                    .foregroundColor(Color(red: 36/225.0, green: 36/225.0, blue: 36/225.0))
-                    Text("색상 선택 완료")
-                    .foregroundColor(.yellow)
-                    .font(.system(size: 20, weight: .medium))
+            
+            NavigationLink(destination: CompleteCardView(viewModel: CompleteCardViewModel(card: Card(albumArtUIImage: viewModel.card.albumArtUIImage, title: viewModel.card.title, singer: viewModel.card.singer, lyrics: viewModel.card.lyrics, cardColor: viewModel.card.cardColor)))) {
+                
+               
+                    ZStack{ Rectangle()
+                            .frame(width: 350, height: 60)
+                            .cornerRadius(30)
+                            .foregroundColor(Color(red: 36/225.0, green: 36/225.0, blue: 36/225.0))
+                        Text("색상 선택 완료")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 20, weight: .medium))
+                    
                 }
             }
-            .padding(.top, 33) }
+            .padding(.top, 33)
+            
+        }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Image(uiImage:viewModel.card.albumArtUIImage).resizable().ignoresSafeArea().scaledToFill().blur(radius: 20))
     }
