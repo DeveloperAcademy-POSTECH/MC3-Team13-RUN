8b78f610441c25b1293f4fc9ad39437032e8cfb1
Jin-s-work <9114jin@hanmail.net>
2023-07-24 11:36:27 +0900

    ✨ FEAT: 가사 선택 후 카드 꾸미기로 넘어가기

--- MoRI/MoRI/VIewModel/SelectLyricsViewModel.swift
+++ MoRI/MoRI/VIewModel/SelectLyricsViewModel.swift
@@ -38,11 +38,11 @@ class SelectLyricsViewModel: ObservableObject {
                     var lyricsText = ""
                     for element in container.array() {
                         let lines = try element.html()
-                            .replacingOccurrences(of: "<br>", with: "")
+                            .replacingOccurrences(of: "<br>", with: " ")
                             .replacingOccurrences(of: "</br>", with: " ")
-                            .replacingOccurrences(of: "<b>", with: "")
+                            .replacingOccurrences(of: "<b>", with: " ")
                             .replacingOccurrences(of: "</b>", with: " ")
-                            .replacingOccurrences(of: "<i>", with: "")
+                            .replacingOccurrences(of: "<i>", with: " ")
                             .replacingOccurrences(of: "</i>", with: " ")
                             .replacingOccurrences(of: "\n\n", with: " ")
                         lyricsText += lines
