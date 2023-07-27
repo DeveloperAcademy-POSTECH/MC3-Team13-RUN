46f3843958b9ecb2412e1f38ab19182deb8f6e33
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-07-27 17:57:44 +0900

    🛠️FIX: CardDetail 프리뷰 삭제

--- MoRI/MoRI/View/CardDetail/CardDetailArt.swift
+++ MoRI/MoRI/View/CardDetail/CardDetailArt.swift
@@ -31,8 +31,3 @@ struct CardDetailArt: View {
         }
     }
 }
-struct CardDetailArt_Previews: PreviewProvider {
-    static var previews: some View {
-        CardDetailArt(viewModel: CardDetailViewModel(card: Card(albumArtUIImage: UIImage(named: "test") ?? UIImage(), title: "커다란", singer: "민수", lyrics: "사랑은 보이지 않는 곳에 흔적을 남기지\n사람은 고스란히 느낄 수가 있지\n서로를 향하는 마음이 진심인지\n참 신기하게도 알 수가 있어", cardColor: .gray)))
-    }
-}
