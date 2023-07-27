c6d0faf8d981f573fe144cb231280f881e07680d
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-27 16:38:28 +0900

    🛠️FIX: 색상 조정
    .

--- MoRI/MoRI/VIew/EditCard/EditCardView.swift
+++ MoRI/MoRI/VIew/EditCard/EditCardView.swift
@@ -48,9 +48,9 @@ struct EditCardView: View {
                     ZStack{ Rectangle()
                             .frame(width: 350, height: 60)
                             .cornerRadius(30)
-                            .foregroundColor(Color(red: 36/225.0, green: 36/225.0, blue: 36/225.0))
+                            .foregroundColor(.gray03Color)
                         Text("색상 선택 완료")
-                            .foregroundColor(Color(red: 0.81, green : 0.92, blue: 0))
+                            .foregroundColor(.primaryColor)
                             .font(.system(size: 20, weight: .medium))
                     
                 }
