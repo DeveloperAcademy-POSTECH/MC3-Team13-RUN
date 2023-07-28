2c19d8bda2b6f0c1226bb1d62a0589342756cbe3
Seokjun Ko <tjrwns1021@gmail.com>
2023-07-28 11:45:21 +0900

    ✨FEAT: 뷰
    공유하기 뷰

---
+++ MoRI/MoRI/View/ShareView.swift
@@ -0,0 +1,51 @@
+//
+//  ShareView.swift
+//  MoRI
+//
+//  Created by Ko Seokjun on 2023/07/28.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    let albumArt: UIImage
+    let singer: String
+    let title: String
+    let cardColor: Color
+    let lyrics: String
+    let lyricsContainerColor: Color
+    let lyricsColor: Color
+    var body: some View {
+        VStack(spacing: 0){
+            ShareTop(albumArt: albumArt, singer: singer, title: title, cardColor: cardColor, lyricsColor: lyricsColor)
+            ZStack{
+                Rectangle()
+                .frame(width: 296, height: 163) .cornerRadius(20)
+                .foregroundColor(lyricsContainerColor)
+                Rectangle()
+                .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
+                .frame(width: 265.55, height: 0.5)
+                .foregroundColor(lyricsContainerColor)
+                .blendMode(.screen)
+                .padding(.top, -82)
+                Text(lyrics)
+                .frame(width: 268.94, alignment: .leading)
+                .foregroundColor(lyricsColor)
+                .font(.custom(FontsManager.Pretendard.regular, size: 14.42))
+                .lineSpacing(12.73)
+                .lineLimit(4)
+            }
+            .compositingGroup()
+        }
+        .compositingGroup()
+        .shadow(radius: 5, x: 8, y: -4)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image(uiImage: albumArt).resizable().ignoresSafeArea().scaledToFill().blur(radius: 20))
+    }
+}
+
+struct ShareView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShareView(albumArt: UIImage(named: "test") ?? UIImage(),singer: "민수", title: "커다란", cardColor: .gray03Color, lyrics: "사랑은 보이지 않는 곳에 흔적을 남기지\n사람은 고스란히 느낄 수가 있지\n서로를 향하는 마음이 진심인지\n참 신기하게도 알 수가 있어\nasdasdsad", lyricsContainerColor: .gray02Color, lyricsColor: .whiteColor)
+    }
+}
