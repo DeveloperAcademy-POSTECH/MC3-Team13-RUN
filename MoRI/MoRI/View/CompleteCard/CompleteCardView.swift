188e583cc187acb89c4c854c79123bb8387bf604
Jin-s-work <9114jin@hanmail.net>
2023-08-02 11:45:18 +0900

    🛠️ FIX : 버튼 위치 수정

--- MoRI/MoRI/View/CompleteCard/CompleteCardView.swift
+++ MoRI/MoRI/View/CompleteCard/CompleteCardView.swift
@@ -64,13 +64,11 @@ struct CompleteCardView: View {
                                 Text("저장하기")
                                     .foregroundColor(.primaryColor)
                                     .font(.custom(FontsManager.Pretendard.medium, size: 20))
-                                // 얘
                             }
                             else {
                                 Image(systemName: "checkmark")
                                     .frame(width: 20, height: 20)
                                     .foregroundColor(.primaryColor)
-                                // 얘도
                             }
                         }
                     }
@@ -90,7 +88,6 @@ struct CompleteCardView: View {
                             Text("메인으로")
                                 .foregroundColor(.gray04Color)
                                 .font(.custom(FontsManager.Pretendard.medium, size: 20))
-                            // 얘
                         }
                     }
                     .padding(.top, 33)
@@ -128,47 +125,6 @@ struct CompleteCardView: View {
                 .foregroundColor(Color(hex: 0x767676))
         }
     }
-    
-    
-    
-    
-//    func shareToInstagramStories() {
-//
-//
-//        let stickerImageData = ExtractImage().renderSticker(view: ShareView(albumArt: viewModel.card.albumArtUIImage, singer: viewModel.card.singer, title: viewModel.card.title, cardColor: viewModel.card.cardColor, lyrics: viewModel.card.lyrics, lyricsContainerColor: viewModel.lyricsContainerColor, lyricsColor: viewModel.lyricsColor), scale: displayScale)?.pngData()
-//        let blurredImage = ExtractImage().renderBackground(view: ShareBack(albumArt: viewModel.card.albumArtUIImage), scale: displayScale)?.pngData()
-//
-//
-//
-//        let urlScheme = URL(string: "instagram-stories://share?source_application=\(Bundle.main.bundleIdentifier ?? "")")
-//        if let urlScheme = urlScheme {
-//            if UIApplication.shared.canOpenURL(urlScheme) {
-//
-//                var pasteboardItems: [[String : Any]]? = nil
-//                if let stickerImageData = stickerImageData {
-//                    pasteboardItems = [
-//                        [
-//                            "com.instagram.sharedSticker.stickerImage": stickerImageData,
-//                            "com.instagram.sharedSticker.backgroundImage": blurredImage as Any
-//
-//                        ]
-//                    ]
-//                }
-//
-//                let pasteboardOptions = [
-//                    UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(60 * 5)
-//                ]
-//
-//                if let pasteboardItems = pasteboardItems {
-//                    UIPasteboard.general.setItems(pasteboardItems, options: pasteboardOptions)
-//                }
-//
-//                UIApplication.shared.open(urlScheme, options: [:], completionHandler: nil)
-//            } else {
-//                print("Something went wrong. Maybe Instagram is not installed on this device?")
-//            }
-//        }
-//    }
     
     
 }
--- MoRI/MoRI/View/SelectLyricsView.swift
+++ MoRI/MoRI/View/SelectLyricsView.swift
@@ -14,7 +14,7 @@ struct SelectLyricsView: View {
     @Binding var songData: SelectedSong
     @Binding var pureData: PureSong
     @State private var selectedTextIndices: [Int] = []
-
+    
     
     @State private var startSelectionIndex: Int?
     @State private var lyricsColor: Color = .whiteColor
@@ -43,51 +43,52 @@ struct SelectLyricsView: View {
                     Text(pureData.name)
                         .font(.custom(FontsManager.Pretendard.medium, size: 14.48276))
                         .foregroundColor(lyricsColor)
-
+                    
                     Text("노래 · " + pureData.artist)
                         .font(.custom(FontsManager.Pretendard.medium, size: 14.48276))
                         .foregroundColor(lyricsColor)
-
+                    
                 }
                 Spacer()
             }
             
             
-            
-            ScrollView {
-                VStack(alignment: .leading){
-                    ForEach(lyricsViewModel.lyrics.indices, id: \.self) { index in
-                        
-                        let text = lyricsViewModel.removeCharactersInsideBrackets(from: lyricsViewModel.lyrics[index])
-                        
-                        Button(action: {
-                            if let start = startSelectionIndex {
-                                let startIndex = min(start, index)
-                                let endIndex = max(start, index)
-                                
-                                if endIndex > startIndex + 3 {
-                                    selectedTextIndices = Array(startIndex...startIndex + 3)
-                                    startSelectionIndex = nil
-                                } else {
-                                    selectedTextIndices = Array(startIndex...endIndex)
-                                    startSelectionIndex = nil
-                                }
-                            }
-                            else {
-                                if selectedTextIndices.contains(index) {
-                                    selectedTextIndices.removeAll { $0 == index }
-                                } else if selectedTextIndices.count >= 4 {
-                                    selectedTextIndices.removeLast()
-                                    selectedTextIndices.insert(index, at: 0) // Insert at the beginning
-                                } else {
-                                    selectedTextIndices.insert(index, at: selectedTextIndices.endIndex) // Insert at the end
-                                    startSelectionIndex = index
-                                }
-                                selectedTextIndices.sort() // Sort the array to maintain the ascending order
-                            }
+            VStack(spacing : 3){
+                
+                ScrollView {
+                    VStack(alignment: .leading){
+                        ForEach(lyricsViewModel.lyrics.indices, id: \.self) { index in
                             
-                        }) {
-                            HStack(alignment: .top) {
+                            let text = lyricsViewModel.removeCharactersInsideBrackets(from: lyricsViewModel.lyrics[index])
+                            
+                            Button(action: {
+                                if let start = startSelectionIndex {
+                                    let startIndex = min(start, index)
+                                    let endIndex = max(start, index)
+                                    
+                                    if endIndex > startIndex + 3 {
+                                        selectedTextIndices = Array(startIndex...startIndex + 3)
+                                        startSelectionIndex = nil
+                                    } else {
+                                        selectedTextIndices = Array(startIndex...endIndex)
+                                        startSelectionIndex = nil
+                                    }
+                                }
+                                else {
+                                    if selectedTextIndices.contains(index) {
+                                        selectedTextIndices.removeAll { $0 == index }
+                                    } else if selectedTextIndices.count >= 4 {
+                                        selectedTextIndices.removeLast()
+                                        selectedTextIndices.insert(index, at: 0) // Insert at the beginning
+                                    } else {
+                                        selectedTextIndices.insert(index, at: selectedTextIndices.endIndex) // Insert at the end
+                                        startSelectionIndex = index
+                                    }
+                                    selectedTextIndices.sort() // Sort the array to maintain the ascending order
+                                }
+                                
+                            }) {
+                                HStack(alignment: .top) {
                                     Text(text)
                                         .padding()
                                         .font(.custom(FontsManager.Pretendard.medium, size: 34))
@@ -98,32 +99,37 @@ struct SelectLyricsView: View {
                                 .frame(maxWidth: 349, alignment : .leading)
                                 .background(selectedTextIndices.contains(index) ? Color.gray.opacity(0.75) : Color.clear)
                                 .cornerRadius(10)
+                            }
                         }
                     }
+                    .padding(.leading, 3)
+                    .onAppear {
+                        lyricsViewModel.fetchHTMLParsingResult(songData)
+                        lyricsColor = chooseLyricsColor(UIImage(data: try! Data(contentsOf: songData.imageUrl!))!)
+                    }
                 }
-                .padding(.leading, 3)
-                .onAppear {
-                    lyricsViewModel.fetchHTMLParsingResult(songData)
-                    lyricsColor = chooseLyricsColor(UIImage(data: try! Data(contentsOf: songData.imageUrl!))!)
+                .border(.red)
+                
+                
+                
+                
+                NavigationLink(destination: EditCardView(viewModel: EditCardViewModel(card: Card(albumArtUIImage:  UIImage(data: try! Data(contentsOf: songData.imageUrl!))!, title: songData.name, singer: songData.artist, lyrics: selectedLyrics, cardColor: .gray)), pureData: $pureData)){
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 350, height: 60)
+                            .cornerRadius(30)
+                            .foregroundColor(.gray03Color)
+                        Text("가사 선택 완료")
+                            .foregroundColor(.primaryColor)
+                            .font(.custom(FontsManager.Pretendard.medium, size: 20))
+                    }
                 }
+                .padding(.top, 33)
+                .padding(.bottom, 22)
+
             }
             
             
-            
-            
-            NavigationLink(destination: EditCardView(viewModel: EditCardViewModel(card: Card(albumArtUIImage:  UIImage(data: try! Data(contentsOf: songData.imageUrl!))!, title: songData.name, singer: songData.artist, lyrics: selectedLyrics, cardColor: .gray)), pureData: $pureData)){
-                ZStack{
-                    Rectangle()
-                        .frame(width: 350, height: 60)
-                        .cornerRadius(30)
-                        .foregroundColor(.gray03Color)
-                    Text("가사 선택 완료")
-                        .foregroundColor(.primaryColor)
-                        .font(.custom(FontsManager.Pretendard.medium, size: 20))
-                    // 바꾸기
-                }
-            }
-            .padding(.top, 33)
         }
         .background(
             AsyncImage(url: songData.imageUrl) { image in
